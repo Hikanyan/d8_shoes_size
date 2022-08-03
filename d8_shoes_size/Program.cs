86ed61691800b7b5c29f90b95b4577ec39e1d722
Hikanyan <hikanyan0131@gmail.com>
2022-08-03 17:44:55 +0900

    プロジェクト ファイルを追加します。

---
+++ d8_shoes_size/Program.cs
@@ -0,0 +1,13 @@
+﻿// https://paiza.jp/works/mondai/drankfast/d8_shoes_size
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var line = Console.ReadLine();
+        Console.WriteLine("XXXXXX");
+    }
+}
